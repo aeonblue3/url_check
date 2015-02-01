3ed7215587bb962eec9da38c48132de43f61a378
Chris Neitzer <chris@neitzer.com>
2015-02-01 17:58:04 -0500

    First commit
    It’s the first one…

---
+++ urlchecker_tests.py
@@ -0,0 +1,15 @@
+import os
+import urlchecker
+import unittest
+import tempfile
+
+classUrlCheckerTestCase(unittest.TestCase):
+  
+  def setUp(self):
+    test = True
+
+  def tearDown(self):
+    test = False
+
+if __name__ == '__main__':
+  unittest.main()
